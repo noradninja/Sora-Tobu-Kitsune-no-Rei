ecff9e1553ddfd4665b88318b8a2eccc640cd4a3
noradninja <josh@whitespacecg.com>
2021-01-21 12:12:16 -0500

    Change to SphereCasts for targeting

--- Assets/Sora Tobu Kitsune-no Rei/Script/Classes/FireControl.cs
+++ Assets/Sora Tobu Kitsune-no Rei/Script/Classes/FireControl.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FireControl : MonoBehaviour {
-	private const string joystick1 = "joystick 1 button ";
-	private const int SQUARE = 2;
+	
+	//privates
 	private RawImage img;
 	private RawImage img2;
 	private Vector3 playerLocation;
-	private Vector3 point;
-	private Vector3 NTLoc;
 	private RaycastHit hit;
 	private float reticleX = 0.0f;
 	private float reticleY = 0.0f;
 	private GameObject obj;
-	private Transform transfer;
+
+	//publics
 	public int objCount = 0;
 	public GameObject missileTarget;
-
 	public int frameCounter = 0;
 	public int lockDelay = 30;
 	public int maxTargets = 4;
@@ -29,20 +27,15 @@ public class FireControl : MonoBehaviour {
 	public GameObject Target2;
 	public GameObject Bullet;
 	public GameObject Missile;
-	public GameObject Player;
-	
 	public GameObject targetVector;
 	private GameObject resetVector;
 	public GameObject Reticle;
-	public float speed = 6.0f; 
 	public Component joyTarget;
 	public List<GameObject> targetList;
-	public static GameObject currtarget;
 	public GameObject audioManager;
 	public List<AudioClip> clipList;
 	public AudioSource audioSource;
 	
-
 	// Use this for initialization
 	void Start () {
 		joyTarget = GameObject.Find ("Main Camera").GetComponent("Joystick_Target");
@@ -52,54 +45,58 @@ public class FireControl : MonoBehaviour {
 		frameCounter = 0;
 		clipList = audioManager.GetComponent<AudioManager>().SFXList;
 		resetVector = GameObject.Find ("Main Camera");
-
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        targetingSystem();
+    }
 
-		//clear target list if it is empty
-		if (targetList.Count <1){
-			targetList.Clear();
-		}
-		playerLocation = GameObject.Find ("ShootPoint").transform.position;
-		Vector3 position = Reticle.transform.position;
-		reticleX = position.x;
-		reticleY = position.y;
-		Ray[] targetingRays = new Ray[9];
-	
-		//set up rays in a rough circle
-			targetingRays [0] = Camera.main.ScreenPointToRay (new Vector3 (reticleX + 16, reticleY, 0));
-			targetingRays [1] = Camera.main.ScreenPointToRay (new Vector3 (reticleX, reticleY + 16, 0));
-			targetingRays [2] = Camera.main.ScreenPointToRay (new Vector3 (reticleX - 16, reticleY, 0));
-			targetingRays [3] = Camera.main.ScreenPointToRay (new Vector3 (reticleX, reticleY - 16, 0));
-			targetingRays [4] = Camera.main.ScreenPointToRay (new Vector3 (reticleX + 8, reticleY + 8, 0));
-			targetingRays [5] = Camera.main.ScreenPointToRay (new Vector3 (reticleX - 8, reticleY - 8, 0));
-			targetingRays [6] = Camera.main.ScreenPointToRay (new Vector3 (reticleX + 8, reticleY - 8, 0));
-			targetingRays [7] = Camera.main.ScreenPointToRay (new Vector3 (reticleX - 8, reticleY + 8, 0));
-			targetingRays [8] = Camera.main.ScreenPointToRay (new Vector3 (reticleX, reticleY, 0));
+	// called in Update for tracking targets
+    private void targetingSystem()
+    {
+        // update needed positions
+        playerLocation = GameObject.Find("ShootPoint").transform.position;
+        Vector3 position = Reticle.transform.position;
+        reticleX = position.x;
+        reticleY = position.y;
 
-		for (int i=0; i<8; i ++){
-			
-			if (Physics.Raycast (targetingRays[i], out hit, 45)) {
-				Debug.DrawRay (targetingRays[i].origin, targetingRays[i].direction * 45, Color.red);
-				GameObject checkHit = GameObject.Find (hit.transform.name);
-				if (checkHit.tag == "Enemy") {
-					print ("There is a new collision with " + checkHit.transform.name + " in front of the reticle!");
-					missileTarget = checkHit;
-				}
-				//else missileTarget = null;
-			}
-		
-		}
-		//reenable missile firing when the list is empty
-		if (targetList.Count < 1){
-			firingMissiles = false;
-		}	
-	}
+        //clear target list if it is empty
+        if (targetList.Count < 1)
+        {
+            targetList.Clear();
+        }
 
-	//deals with lock on system
-	void fireControl(){ //called from Buttons.cs when fire button is down
+        // set up targeting ray
+        Ray targetingRay = new Ray();
+        // cast ray from camera through location of targeting reticle sprite
+        targetingRay = Camera.main.ScreenPointToRay(new Vector3(reticleX, reticleY, 0));
+        // draw for debug purposes
+        Debug.DrawRay(targetingRay.origin, targetingRay.direction * 45, Color.red);
+        // cast a sphere along the length of the ray
+        if (Physics.SphereCast(targetingRay, 0.5f, out hit))
+        {
+            // get the name of the object we hit
+            GameObject checkHit = GameObject.Find(hit.transform.name);
+            // is it an enemy
+            if (checkHit.tag == "Enemy")
+            {
+                // debug print 
+                print("There is a new collision with " + checkHit.transform.name + " in front of the reticle!");
+                // make the enemy a target for a missile
+                missileTarget = checkHit;
+            }
+        }
+        //reenable missile firing when the list is empty
+        if (targetList.Count < 1)
+        {
+            firingMissiles = false;
+        }
+    }
+
+    //deals with lock on system
+    void fireControl(){ //called from Buttons.cs when fire button is down
 		
 		//clear list and reset target vector 
 		if (targetList.Count <1){
