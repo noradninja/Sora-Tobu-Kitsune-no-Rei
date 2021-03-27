177af5d5a4cfcbeeaccd25d31b6ccf43478d495d
noradninja <josh@whitespacecg.com>
2021-03-27 17:10:08 -0400

    Twaek flash rate and lerp shutoff colors

--- Assets/Sora Tobu Kitsune-no Rei/Script/Classes/Bezerk_Blinker.cs
+++ Assets/Sora Tobu Kitsune-no Rei/Script/Classes/Bezerk_Blinker.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Bezerk_Blinker : MonoBehaviour {
 
 	public Color baseColor;
+	public Color dimColor;
 	public Color brightColor;
 	public GameObject eventManager;
 	public float repeatRate;
@@ -16,26 +17,31 @@ public class Bezerk_Blinker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (eventManager.GetComponent<Link_System>().newValue >= 0.25f){
+		if (eventManager.GetComponent<Link_System>().newValue >= 0.25f){ //is meter > 25% full
 			Blinker();
 		}
 	}
 
 	void Blinker(){
-		StartCoroutine(colorAnimator(baseColor, brightColor, repeatRate));
+		StartCoroutine(colorAnimator(dimColor, brightColor, repeatRate)); //blinkenlights
 	}
 
 	IEnumerator colorAnimator (Color startColor, Color endColor, float duration){
-		if(eventManager.GetComponent<Link_System>().newValue < 0.25f){
-			GetComponent<Image>().color = baseColor;
-			yield break;
-		}
 		float time = 0;
+		if(eventManager.GetComponent<Link_System>().newValue < 0.25f){ //if we're blinking and meter drop below 25%, lerp back to red
+			while (time < duration){
+				Color currentColor = GetComponent<Image>().color;
+				GetComponent<Image>().color = Color.Lerp(currentColor, baseColor, time/duration);
+				time += Time.deltaTime;
+				yield return null;
+			}
+			
+		}
 		while (time < duration){
-			GetComponent<Image>().color = Color.Lerp(startColor, endColor, time/duration);
+			GetComponent<Image>().color = Color.Lerp(startColor, endColor, time/duration); //lerp the colors from dark to light
 			time += Time.deltaTime;
 			yield return null;
 		}
-	StartCoroutine(colorAnimator(brightColor, baseColor, repeatRate));	
+	StartCoroutine(colorAnimator(brightColor, dimColor, repeatRate)); //call the routine again to lerp the color back from light to dark, this will cycle until bezerk drops below 25%
 	}
 }
